1cb1c4019c0c5b37fee1c88d4f60dc0c04349d19
Matthew Falbo <matthew.falbo@gmail.com>
2014-04-07 13:44:53 -0600

    Volunteer section and Summary alert()

--- main.js
+++ main.js
@@ -39,3 +39,61 @@ for(var i=1; i<=numVictims; i++){
 // console.log(vicNameArray);
 // console.log(vicPhoneNumberArray);
 // console.log(vicStreetArray);
+
+var numVolunteers;
+
+while(isNaN(numVolunteers)){
+
+numVolunteers = prompt("How many disaster volunteers do you have? ");
+
+if(isNaN(numVolunteers)){
+	alert("Please use numerals.");
+}
+
+!isNaN(+numVolunteers);
+}
+
+// console.log(numVolunteers);
+
+// Retrieve and Store Volunteer Information
+
+// Declare empty volunteer arrays
+	var volNameArray = [];
+	var volPhoneNumberArray = [];
+	var volStreetArray = [];
+
+for(var i=1; i<=numVolunteers; i++){
+
+	// console.log(i);
+
+	var volName = prompt("Name of volunteer #" + i);
+	volNameArray.push(volName)
+
+	var volPhoneNumber = prompt("Phone number of volunteer #" + i);
+	volPhoneNumberArray.push(volPhoneNumber)
+
+	var volStreet = prompt("Street of volunteer #" + i);
+	volStreetArray.push(volStreet)
+}
+
+// console.log(volNameArray);
+// console.log(volPhoneNumberArray);
+// console.log(volStreetArray);
+
+alert("There are " + numVictims + "victims.\n"
+	+ "There are " + numVolunteers + "volunteers.\n"
+	+ "Victim #1: " + vicNameArray[0] + " " + vicPhoneNumberArray[0] + " " + vicStreetArray[0] + "\n"
+	+ "Victim #2: " + vicNameArray[1] + " " + vicPhoneNumberArray[1] + " " + vicStreetArray[1] + "\n"
+	+ "Volunteer #1: " + volNameArray[0] + " " + volPhoneNumberArray[0] + " " + volStreetArray[0] + "\n"
+	+ "Volunteer #2: " + volNameArray[1] + " " + volPhoneNumberArray[1] + " " + volStreetArray[1] + "\n")
+
+
+
+
+
+
+
+
+
+
+
